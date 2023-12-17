fbe39f2fbdb0b3065fb37fa82dd39411bc626739
tirouzh <ctirouzh@gmail.com>
2023-12-17 22:30:52 +0330

    Refactor config.Get() Postgres test

--- config/config_test.go
+++ config/config_test.go
@@ -6,20 +6,21 @@ import (
 	"web-service-gin/config"
 )
 
-func TestGet(t *testing.T) {
+func TestGet_Postgres(t *testing.T) {
+	// Happy test case
 	cfg := config.Get()
 	if cfg.Postgres.Host != "localhost" {
 		t.Errorf("expected postgres.host = %s, got %s", "localhost", cfg.Postgres.Host)
 	}
-
+	// Test case 1
 	cfg.Postgres.Host = "something-else" // Developer mistake in a goroutine!
 	// Get config again in another goroutine
 	cfg2 := config.Get()
 	if cfg2.Postgres.Host != "localhost" {
 		t.Errorf("expected postgres.host = %s, got %s", "localhost", cfg2.Postgres.Host)
 	}
-
-	err := os.Setenv("DB_HOST", "updated-db-host-env") // Developer mistake in a goroutine!
+	// Test case 2
+	err := os.Setenv("DB_HOST", "updated-db-host-value-using-os.Setenv") // Developer mistake in a goroutine!
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
