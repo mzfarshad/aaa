bd8a993f0b96fd7215efe9a044d62bc77351bced
tirouzh <ctirouzh@gmail.com>
2023-12-17 23:08:03 +0330

    Minorimprovment in  exercise todo comment

--- config/postgres_test.go
+++ config/postgres_test.go
@@ -6,5 +6,5 @@ func TestPostgres_fromEnv(t *testing.T) {
 	// TODO: @Farshad
 	// 1. Use os.Setenv to set "DB_HOST","DB_PORT","DB_USER", and "DB_PASS" to some values
 	// 2. Assign a variable (for example, p) to new(postgres).fromEnv()
-	// 3. Test if p.Host == os.Getenv("DB_HOST"), and continue for other keys...
+	// 3. Test failed if p.Host != os.Getenv("DB_HOST"), and so on...
 }
