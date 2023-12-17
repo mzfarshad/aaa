40e0de205bbca3eae997740a3727b82e59958075
tirouzh <ctirouzh@gmail.com>
2023-12-17 23:04:54 +0330

    TODO @Farshad: Testing config.postgres.fromEnv()

---
+++ config/postgres_test.go
@@ -0,0 +1,10 @@
+package config_test
+
+import "testing"
+
+func TestPostgres_fromEnv(t *testing.T) {
+	// TODO: @Farshad
+	// 1. Use os.Setenv to set "DB_HOST","DB_PORT","DB_USER", and "DB_PASS" to some values
+	// 2. Assign a variable (for example, p) to new(postgres).fromEnv()
+	// 3. Test if p.Host == os.Getenv("DB_HOST"), and continue for other keys...
+}
